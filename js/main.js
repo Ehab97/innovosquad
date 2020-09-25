846f1609776b683a20d2f4d3c1a14de767fe94eb
Ehab Reda <ehabred04@gmail.com>
2020-09-25 11:48:14 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var project=document.querySelectorAll('.portfolio .project');
+  var tab=document.querySelectorAll('.tabs li');
+  var detail=document.querySelectorAll('.tabs li .details');
+  // console.log(...tab);
+//   console.log(...project);
+var toggle = document.querySelector('.toggle');
+    var link = document.querySelectorAll('.link');
+    // const styleSetter = (ele) => {
+    //     return (prop, value) => {
+    //         ele.style[prop] = value;
+    //     }
+    // }
+
+    // function toggle_visibility(ele) {
+    //     var e = document.getElementsByClassName('ele');
+    //     if (e.style.display == 'block')
+    //         e.style.display = 'none';
+    //     else
+    //         e.style.display = 'block';
+    // }
+    toggle.onclick = () => {
+        if (link[0].style.display == 'block') {
+            for (let i = 0; i < link.length; i++) {
+                link[i].style.display = 'none';
+            }
+        } else {
+            for (let i = 0; i < link.length; i++) {
+                link[i].style.display = 'block';
+            }
+        }
+    }
+ for (let index = 0; index < tab.length; index++) {
+  tab[index].onclick=(e)=>{
+    console.log(e.getAttribyte());
+    detail.forEach(element => {
+     console.log(element);
+   });
+}
+   
+ }
+  for (let index = 0; index < project.length; index++) {
+    // project[index].style.backgroundImage=`url(../imgaes/projects/${index}.jpg)`;
+    project[index].setAttribute('style',`background: url(imgaes/projects/${index+1}.jpg) center center no-repeat;
+    background-size: cover;`);
+    // console.log(project[index]);
+  }
+
+});
+new Vue({
+    el: '#app',
+    data: {
+        
+    },
+    methods: {
+
+    },
+
+});
